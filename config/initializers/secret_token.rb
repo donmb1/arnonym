6915dbc50d59a0f9380a24577aef6f8620e93774
Martin <mbraun@monkeyandco.de>
2013-11-11 22:12:50 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arnonym::Application.config.secret_key_base = 'a93a9453b3e13e6ab29999d613527521b0d37533eee41cded3eb1bb2edab3633476ab277b8fd1d58518159270af0e4079f5a0702643b6488ad232cd4ed81256f'
