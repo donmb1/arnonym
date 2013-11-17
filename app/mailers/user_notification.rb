cc741cb0810c082f00947ca27b3d84ac36d212e2
Martin <mbraun@monkeyandco.de>
2013-11-17 22:29:32 +0100

    mails fixes

--- app/mailers/user_notification.rb
+++ app/mailers/user_notification.rb
@@ -3,19 +3,22 @@ class UserNotification < ActionMailer::Base
   
   def notify_user(email, code)
     @code = code
+    @poll = Poll.find_by_code(@code)
     mail(:to => email,
-      :subject => "You've been invited to a poll.")
+      :subject => "You've been invited to a POLLIFY poll.")
   end
   
   def notify_admin(email, code)
     @code = code
+    @poll = Poll.find_by_code(@code)
     mail(:to => email,
-      :subject => "Your poll was created.")
+      :subject => "Your new poll at POLLIFY was created.")
   end
   
   def notify_admin_of_comment(email, code)
     @code = code
+    @poll = Poll.find_by_code(@code)
     mail(:to => email,
-      :subject => "New comment and rating received.")
+      :subject => "New comment and rating on your POLLIFY poll received.")
   end
 end
